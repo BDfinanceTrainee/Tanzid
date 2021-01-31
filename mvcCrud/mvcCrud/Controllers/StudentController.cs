b3cceacf2c8ab9ce3dd41191a17d1eb3672916c5
BDfinancetrainee <bdfinancetrainee2020@gmail.com>
2021-01-31 17:09:46 +0600

    Mvc crud

--- mvcCrud/mvcCrud/Controllers/StudentController.cs
+++ mvcCrud/mvcCrud/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using mvcCrud.Context;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,24 +12,53 @@ namespace mvcCrud.Controllers
     {
         // GET: Student
         db_testEntities dbobj = new db_testEntities();
-        public ActionResult Student()
+        public ActionResult Student(tbl_Student obj)
         {
-            return View();
+                return View(obj);
+            
         }
         [HttpPost]
         public ActionResult AddStudent(tbl_Student model)
         {
             tbl_Student obj = new tbl_Student();
-            obj.Name = model.Name;
-            obj.Fname = model.Fname;
-            obj.Email = model.Name;
-            obj.Mobile = model.Mobile;
-            obj.Description = model.Description;
+            if (ModelState.IsValid)
+            {
+                obj.ID = model.ID;
+                obj.Name = model.Name;
+                obj.Fname = model.Fname;
+                obj.Email = model.Email;
+                obj.Mobile = model.Mobile;
+                obj.Description = model.Description;
 
-            dbobj.tbl_Student.Add(obj);
-            dbobj.SaveChanges();
-
+                if (model.ID == 0)
+                {
+                    dbobj.tbl_Student.Add(obj);
+                    dbobj.SaveChanges();
+                }
+                else
+                {
+                    dbobj.Entry(obj).State = EntityState.Modified;
+                    dbobj.SaveChanges();
+                }
+                ModelState.Clear();
+            }
+            
             return View("Student");
+            }
+        
+        public ActionResult StudentList()
+        {
+            var res = dbobj.tbl_Student.ToList();
+            return View(res);
         }
+        public ActionResult Delete(int id)
+        {
+            var res = dbobj.tbl_Student.Where(x => x.ID == id).First();
+            dbobj.tbl_Student.Remove(res);
+            dbobj.SaveChanges();
+            var list = dbobj.tbl_Student.ToList();
+            return View("StudentList",list);
+        }
+        
     }
 }
