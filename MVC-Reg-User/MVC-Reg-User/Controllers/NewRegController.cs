34434789f7e8c2e6ac5e13dc326e3dbcd2cbb8fd
BDfinancetrainee <bdfinancetrainee2020@gmail.com>
2021-02-01 17:27:46 +0600

    MVC file upload

---
+++ MVC-Reg-User/MVC-Reg-User/Controllers/NewRegController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Reg_User.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace MVC_Reg_User.Controllers
+{
+    public class NewRegController : Controller
+    {
+        // GET: NewReg
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(UserClass uc,HttpPostedFileBase file)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery = "insert into[dbo].[MVCregUser](Uname,NID,Date,Uimg) values(@Uname,@NID,@Date,@Uimg)";
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            return View();
+            sqlconn.Open();
+            sqlcomm.Parameters.AddWithValue("@Uname", uc.Uname);
+            sqlcomm.Parameters.AddWithValue("@NID", uc.NID);
+            sqlcomm.Parameters.AddWithValue("@Date", uc.Date);
+            sqlcomm.Parameters.AddWithValue("@Uimg", uc.Uimg);
+            if(file!=null && file.ContentLength>0)
+            {
+                String filename = Path.GetFileName(file.FileName);
+                string imgpath = Path.Combine(Server.MapPath("~/User-Images/"), filename);
+                file.SaveAs(imgpath);
+                
+            }
+            sqlcomm.Parameters.AddWithValue("@Uimg", "~/User-Images/"+file.FileName);
+            sqlcomm.ExecuteNonQuery();
+            sqlconn.Close();
+            ViewData["Message"] = "User Record" + uc.Uname + "Is saved successfully!";
+            return View();
+        }
+    }
+}
---
+++ MVC-Reg-User/MVC-Reg-User/Models/UserClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace MVC_Reg_User.Models
+{
+    public class UserClass
+    {
+        [Required(ErrorMessage ="Enter Username!")]
+        [Display(Name = "Enter Username :" )]
+        [StringLength(maximumLength:7,MinimumLength =3,ErrorMessage ="Username Length must be max 7 & min 3")]
+        public string Uname { get; set; }
+
+        [Required(ErrorMessage = "Enter NID Number!")]
+        [Display(Name = "Enter NID :")]
+        public string NID { get; set; }
+        [Required(ErrorMessage = "Enter Date!")]
+        [Display(Name = "Enter Date :")]
+        public string Date { get; set; }
+        [Required(ErrorMessage = "Upload Profile Image!")]
+        [Display(Name = "Profile Image :")]
+        public string Uimg { get; set; }
+    }
+}
